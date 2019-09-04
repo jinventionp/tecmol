76eb644a49c166e1ad797772e4c9c9c7499d9729
jinventionp <elpana_@hotmail.com>
2019-09-03 20:01:04 -0500

    Add Logout Users

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -123,6 +123,11 @@ class UsersController extends AppController
         }
     }
 
+    public function logout()
+    {
+        return $this->redirect($this->Auth->logout());
+    }
+
 
     public function recovery()
     {
